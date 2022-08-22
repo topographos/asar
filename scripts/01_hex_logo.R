389eacde6a37dca87e3607eb44388943b469033f
Michal Michalski <michalski82@gmail.com>
2022-08-22 11:51:28 +0100

    commit

--- scripts/01_hex_logo.R
+++ scripts/01_hex_logo.R
@@ -1,5 +1,5 @@
-install.packages("hexSticker")
-install.packages("wesanderson")
+#install.packages("hexSticker")
+#install.packages("wesanderson")
 
 library(ggplot2)
 library(hexSticker)
@@ -19,7 +19,7 @@ survey = st_read("data/data.gpkg", layer = "survey_extent")
 p = ggplot() +
   geom_sf(data = sites, 
           aes(size = size_ha),
-          color = "#645f55", 
+          color = "#c03728", 
           fill = "#645f55",
           alpha = .1) +
   theme_minimal() +
@@ -41,16 +41,17 @@ royal = wes_palette("Royal1")
 
 
 sticker(p,
-        package="Archaeological \n Spatial Analysis \n in R",
+        package="ASAR",
         s_x=1, s_y= 0.9, s_width= 1.75, s_height= 1.75,
         p_family = "sans-serif",
         p_size= 5, 
-        p_y = 1,
-        p_color = "#A84268",
+        p_y = 0.6,
+        p_x = 0.5,
+        p_color = "#828585",
         h_fill =  "#C0BCB5",
-        h_color = "#A84268",
+        h_color = "#828585",
         url = "https://topographos.github.io/asar/",
-        u_color	= "#A84268",
+        u_color	= "#828585",
         u_size = 1.2,
         filename="assets/hex_logo.png")
 
